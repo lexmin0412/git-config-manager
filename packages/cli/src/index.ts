17fd384f48748f8e55740bae0dde26b567250229
lexmin0412 <zhangle_dev@outlook.com>
2026-03-09 18:18:56 +0800

    refactor(cli): 重构命令注册逻辑以消除重复代码
    将重复的命令注册代码重构为循环遍历命令配置数组
    统一错误处理逻辑，避免每个命令单独处理
    提升代码可维护性和可读性

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -30,151 +30,84 @@ ${pc.green(artText)}`);
 // 解决事件监听过多，运行命令后弹出警告信息的问题（默认最大监听器数量为 10 个）
 EventEmitter.setMaxListeners(20)
 
-program
-	.version(pkgJson.version)
-	.command('use <alias>')
-	.description('use git user config by alias')
-	.action((alias: string) => {
-		try {
-			use(alias)
-		} catch (error) {
-			console.error(error)
-			process.exit(1)
-		}
-	})
+const commands = [
+	{
+		command: 'use <alias>',
+		description: 'use git user config by alias',
+		action: use
+	},
+	{
+		command: 'add',
+		description: 'add user config',
+		action: add
+	},
+	{
+		command: 'remove',
+		alias: 'rm',
+		description: 'remove user config',
+		action: remove
+	},
+	{
+		command: 'list',
+		alias: 'ls',
+		description: 'list all configs in global config file',
+		action: list
+	},
+	{
+		command: 'scan',
+		alias: 'sc',
+		description: 'scan all git project\'s config in directory',
+		action: scan
+	},
+	{
+		command: 'doctor',
+		description: 'verify if your git config in current workspace is correct',
+		action: doctor
+	},
+	{
+		command: 'upgrade',
+		description: 'upgrade version of gcm self',
+		action: upgrade
+	},
+	{
+		command: 'get-config <type>',
+		description: 'get configuration',
+		action: getConfig
+	},
+	{
+		command: 'set-config <type>',
+		description: 'update configuration',
+		action: setConfig
+	},
+	{
+		command: 'sync',
+		description: 'sync config to remote',
+		action: sync
+	},
+	{
+		command: 'current',
+		alias: 'cur',
+		description: 'get current git config in config file',
+		action: current
+	}
+]
 
-program
-	.version(pkgJson.version)
-	.command('add')
-	.description('add user config')
-	.action(() => {
-		try {
-			add()
-		} catch (error) {
-			console.error(error)
-			process.exit(1)
-		}
-	})
+program.version(pkgJson.version)
 
-program
-	.version(pkgJson.version)
-	.command('remove')
-	.alias('rm')
-	.description('remove user config')
-	.action(() => {
+commands.forEach(({ command, alias, description, action }) => {
+	const cmd = program.command(command).description(description)
+	if (alias) {
+		cmd.alias(alias)
+	}
+	cmd.action(async (...args: any[]) => {
 		try {
-			remove()
+			// @ts-ignore
+			await action(...args)
 		} catch (error) {
 			console.error(error)
 			process.exit(1)
 		}
 	})
-
-program
-	.version(pkgJson.version)
-	.command('list')
-	.alias('ls')
-	.description('list all configs in global config file')
-	.action(() => {
-		try {
-			list()
-		} catch (error) {
-			console.error(error)
-			process.exit(1)
-		}
-	})
-
-program
-	.version(pkgJson.version)
-	.command('scan')
-	.alias('sc')
-	.description('scan all git project\'s config in directory')
-	.action(() => {
-		try {
-			scan()
-		} catch (error) {
-			console.error(error)
-			process.exit(1)
-		}
-	})
-
-program
-	.version(pkgJson.version)
-	.command('doctor')
-	.description('verify if your git config in current workspace is correct')
-	.action(() => {
-		try {
-			doctor()
-		} catch (error) {
-			console.error(error)
-			process.exit(1)
-		}
-	})
-
-program
-	.version(pkgJson.version)
-	.command('upgrade')
-	.description('upgrade version of gcm self')
-	.action(() => {
-		try {
-			upgrade()
-		} catch (error) {
-			console.error(error)
-			process.exit(1)
-		}
-	})
-
-program
-	.version(pkgJson.version)
-	.command('get-config <type>')
-	.description('get configuration')
-	.action((type: 'sync') => {
-		try {
-			getConfig(type)
-		} catch (error) {
-			console.error(error)
-			process.exit(1)
-		}
-	})
-
-program
-	.version(pkgJson.version)
-	.command('set-config <type>')
-	.description('update configuration')
-	.action((type: 'sync') => {
-		try {
-			setConfig(type)
-		} catch (error) {
-			console.error(error)
-			process.exit(1)
-		}
-	})
-
-program
-	.version(pkgJson.version)
-	.command('sync')
-	.description('sync config to remote')
-	.action(() => {
-		try {
-			sync()
-		} catch (error) {
-			console.error(error)
-			process.exit(1)
-		}
-	})
-
-program
-	.version(pkgJson.version)
-	.command('current')
-	.alias('cur')
-	.description('get current git config in config file')
-	.action(() => {
-		try {
-			current()
-		} catch (error) {
-			console.error(error)
-			process.exit(1)
-		}
-	})
+})
 
 program.parse()
